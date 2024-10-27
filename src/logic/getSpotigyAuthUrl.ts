8c957d0aeb7f333b5b28d51b90821ab6d7497763
Alina-02 <alinamontma@gmail.com>
2024-10-27 21:48:53 +0100

    delete redirect uri

--- src/logic/getSpotigyAuthUrl.ts
+++ src/logic/getSpotigyAuthUrl.ts
@@ -1,6 +1,4 @@
-const REDIRECT_URI = `${window.location.protocol}${window.location.hostname}/callback`; // Same as the one configured in Spotify Dashboard
-console.log(window.location.protocol);
-console.log(window.location.hostname);
+//const REDIRECT_URI = `${window.location.protocol}${window.location.hostname}/callback`; // Same as the one configured in Spotify Dashboard
 const SCOPES = ['user-read-private', 'user-library-read']; // Add other scopes if needed
 
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID as string;
@@ -10,7 +8,6 @@ export function getSpotifyAuthUrl(): string {
   const queryParams = new URLSearchParams({
     client_id: CLIENT_ID,
     response_type: 'code',
-    redirect_uri: REDIRECT_URI,
     scope: SCOPES.join(' '),
     state: Math.random().toString(36).substring(2, 15), // Optional, adds security
   });
